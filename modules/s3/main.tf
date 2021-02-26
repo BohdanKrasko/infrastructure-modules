3e3155bf3d5827be705eeab96294fd6cc3527c0b
Bohdan <krasko200006@gmail.com>
2021-02-26 16:46:57 +0200

    fix S3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,16 @@
 resource "aws_s3_bucket" "b" {
   bucket = var.s3_bucket_name
   acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = aws_s3_bucket.b.id
+
   policy = <<POLICY
 {
     "Version": "2012-10-17",
@@ -15,13 +25,7 @@ resource "aws_s3_bucket" "b" {
     ]
 }
 POLICY
-
-  website {
-    index_document = "index.html"
-  }
-  force_destroy = true
 }
-
 //resource "null_resource" "example1" {
 //  provisioner "local-exec" {
 //    command = "cd client && REACT_APP_HOST={var.go_server}  yarn build && aws s3 sync build/ s3://${aws_s3_bucket.b.bucket}" 
